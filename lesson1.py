b7e71d98b59ed63d2a39e0f0e42b3cd5e7930559
maddix510 <clouatre47951@students.apsb.org>
2025-11-07 21:25:45 +0000

    Added lesson2.py and learned about variables, data types, and tpye()

--- lesson1.py
+++ lesson1.py
@@ -1,6 +1,4 @@
 # Lesson 1: Printing and Comments (with blank lines)
-# print ("") is how you get Python to say what you want, you type what ever you want in the "" and it'll say it in the terminal
-# tpye \n at the end of what your saying but still in the "" to create a space in between your lines (do this everytime you want to create a space in between your lines)
 
 print ("Hello, world!\n")
 print ("My name is Maddix\n")
---
+++ lesson2.py
@@ -0,0 +1,20 @@
+# Lesson 2: Variables and Data Types
+
+name = "maddix"
+age = 13
+goal = "make a game in Python"
+favorite_game = "Borderlands 2"
+random_number = 145
+
+print ("Hi! My name is", name)
+print ("I'm", age, "years old.")
+print ("One day I wat to", goal)
+print ("My favorite game is", favorite_game)
+print ("My lucky number is", random_number)
+
+likes_Python = True
+print ("Do I like Python?", likes_Python)
+
+print (type(name))
+print (type(age))
+print (type(likes_Python))
