b5619a9b6ebd11207fd0b66ad53dfe6a24a4e103
maddix510 <clouatre47951@students.apsb.org>
2025-11-07 04:12:46 +0000

    Completed Lesson 1: Printing and Comments

---
+++ lesson1.py
@@ -0,0 +1,9 @@
+# Lesson 1: Printing and Comments (with blank lines)
+# print ("") is how you get Python to say what you want, you type what ever you want in the "" and it'll say it in the terminal
+# tpye \n at the end of what your saying but still in the "" to create a space in between your lines (do this everytime you want to create a space in between your lines)
+
+print ("Hello, world!\n")
+print ("My name is Maddix\n")
+print ("I'm learning Python step by step\n")
+print ("My favorite game is Borderlands 2\n")
+print ("One day I want to make my own game\n")
