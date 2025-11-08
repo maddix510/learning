0c01c7af2b21c62a88e63bb90d90144d686c2b40
maddix510 <clouatre47951@students.apsb.org>
2025-11-08 15:47:31 +0000

    Add Lesson 3 - Getting User Input

---
+++ lesson3.py
@@ -0,0 +1,12 @@
+# Lesson 3: Input and Interaction
+
+name = input("What's your name? ")
+favorite_game = input("What's your favorite game? ")
+goal = input("What do you want to make with Python? ")
+has_pet = input("Do you have a pet? ")
+
+print("\nNice to meet you,", name + "!")
+print("I also think", favorite_game, "is awsome.")
+print("Good luck with your goal to", goal + "!")
+print("Oh cool,", has_pet, "sounds fun!")
+print("Thanks for chatting with me", name + "!")
