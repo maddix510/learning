da86f17a7f884c25550651095470668aeca109b8
maddix510 <clouatre47951@students.apsb.org>
2025-11-11 00:31:56 +0000

    completed lesson 5: If Statements

---
+++ lesson5.py
@@ -0,0 +1,15 @@
+# Lesson 5: If Statements
+
+favorite_game = input ("What's your favorie game? ")
+
+if favorite_game.lower() == "borderlands 2":
+    print("Wow, great choice")
+else:
+    print("Cool, I like that game too!")
+
+Like_Python = input ("Do you like Python? ")
+
+if Like_Python.lower() == "yes":
+    print("Yay! Python is awsome!")
+else:
+    print("Don't worry, everyone has their favorite language.")
