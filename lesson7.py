06743efc5576110f1006bed38fd3d291cbf39b40
maddix510 <clouatre47951@students.apsb.org>
2025-11-14 01:42:43 +0000

    Added Lesson 7

---
+++ lesson7.py
@@ -0,0 +1,48 @@
+# Lesson 7: While Loops
+
+number = 1
+
+while number <= 3:
+    print("Hello, Maddix! Loop", number)
+    number = number + 1
+
+# Mini Callege (customize)
+
+name = input("What's your name? ")
+
+count = 1
+
+while count <= 5:
+    print(name)
+    count = count + 1
+
+# Part 2 (counting down)
+
+# Countdown example
+
+count = 5
+
+while count > 0:
+    print("Countdown", count)
+    count = count - 1
+
+print("Blast off!")
+
+# The for Loop
+
+# Practice
+
+for i in range(5):
+    print("Loop number:", i)
+
+# Lesson: 7 For Loop
+
+print("Counting to 5:")
+
+for number in range(1, 6):
+    print(number)
+
+# Tiny Challenge
+
+for i in range(3):
+    print("Maddix")
